02133ccd83f95befff8f41cb0efaf813d4c44022
Shrewkin <strizhevp@gmail.com>
2021-06-16 03:14:54 -0700

    Implemented Giphy API
    Implemented the Giphy api into the bot and now allows for gif search commands.

--- Code/Bot.py
+++ Code/Bot.py
@@ -1,27 +1,59 @@
-import discord
-import os
+# Imports
+from discord import colour
+from discord.ext import commands
+from pprint import pprint
 import linecache
+import discord
+import aiohttp
+import random
+import json
+import os
 
-client = discord.Client()
+from discord.ext.commands.core import command
 
-@client.event
+# API Url's
+giphyURL = "http://api.giphy.com/v1/gifs/search"
+
+# Command Initialization
+bot = commands.Bot(command_prefix=';')
+
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# Test Case
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
 
-    if message.content.startswith(';hello'):
-        await message.channel.send('Hello!')
+# Giphy Implementation
+giphyKey = linecache.getline('Fonzie-Bot\Authentication Keys\Authentication.txt', 17)
+giphyKey.rstrip()
 
+@bot.command(pass_context=True)
+async def giphy(ctx, *, search):
+    embed = discord.Embed(colour=discord.Colour.blue())
+    session = aiohttp.ClientSession()
+
+    # If the search is empty return a random gif
+    if search == '':
+        response = await session.get('https://api.giphy.com/v1/gifs/random?api_key=' + 'IY7W2PRaObx8r8N5LJO4Y3zFGL3MgaBJ')
+        data = json.loads(await response.text())
+        embed.set_image(url=data['data']['images']['original']['url'])
+    else: # If the search is not empty then implement it into the search url
+        search.replace(' ', '+')
+        response = await session.get('http://api.giphy.com/v1/gifs/search?q=' + search + '&api_key=' + 'IY7W2PRaObx8r8N5LJO4Y3zFGL3MgaBJ' + '&limit=10')
+        data = json.loads(await response.text())
+        gif_choice = random.randint(0, 9)
+        embed.set_image(url=data['data'][gif_choice]['images']['original']['url'])
+
+    await session.close()
+    await ctx.send(embed=embed)
 
 # Code to get token for bot
 token = linecache.getline('Fonzie-Bot\Authentication Keys\Authentication.txt', 8)
 
 # Clean up token so it has no trailing newline
 token.rstrip()
-print(token)
 
-client.run(token)
+bot.run(token)
