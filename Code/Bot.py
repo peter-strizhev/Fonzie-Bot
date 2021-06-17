def08026d17aab913b961a11c16cdf4d9c8c091e
Shrewkin <strizhevp@gmail.com>
2021-06-16 23:16:21 -0700

    Implemented Detect feature
    Implemented a detection feature where the user can type ;detect {user} {term} and it will then display one of 3 messages.

--- Code/Bot.py
+++ Code/Bot.py
@@ -2,10 +2,10 @@
 from discord import channel, colour
 from discord.ext import commands
 from pprint import pprint
+from numpy import random
 import linecache
 import discord
 import aiohttp
-import random
 import json
 import os
 
@@ -21,35 +21,16 @@ bot = commands.Bot(command_prefix=';')
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
 
-# Test Case
+# Sandbox -----------------------------------------------------------------------------------------------------------------------------------------
 @bot.command()
-async def test(ctx, arg):
-    await ctx.send(arg)
+async def test(ctx, user: discord.Member, arg2):
+    if arg2:
+        await ctx.send(user.mention + ' ' + arg2)
+    else:
+        await ctx.send('You dumbass, this command requires two arguments')
 
-# Giphy Implementation
-giphyKey = linecache.getline('Fonzie-Bot\Authentication Keys\Authentication.txt', 17)
-giphyKey.rstrip()
 
-@bot.command(pass_context=True)
-async def giphy(ctx, *, search):
-    embed = discord.Embed(colour=discord.Colour.blue())
-    session = aiohttp.ClientSession()
-
-    # If the search is empty return a random gif
-    if search == '':
-        response = await session.get('https://api.giphy.com/v1/gifs/random?api_key=' + 'IY7W2PRaObx8r8N5LJO4Y3zFGL3MgaBJ')
-        data = json.loads(await response.text())
-        embed.set_image(url=data['data']['images']['original']['url'])
-    else: # If the search is not empty then implement it into the search url
-        search.replace(' ', '+')
-        response = await session.get('http://api.giphy.com/v1/gifs/search?q=' + search + '&api_key=' + 'IY7W2PRaObx8r8N5LJO4Y3zFGL3MgaBJ' + '&limit=10')
-        data = json.loads(await response.text())
-        gif_choice = random.randint(0, 9)
-        embed.set_image(url=data['data'][gif_choice]['images']['original']['url'])
-
-    await session.close()
-    await ctx.send(embed=embed)
-
+# Random meme responses ---------------------------------------------------------------------------------------------------------------------------
 @bot.event
 async def on_message(message):
     if message.author == bot.user:  # skip bot messages
@@ -60,11 +41,53 @@ async def on_message(message):
         await message.channel.send('https://cdn.discordapp.com/attachments/767899814402326559/854588332247744572/1596575456116-1.png')
 
     await bot.process_commands(message)  # to allow other commands
+    
 
-# Code to get token for bot
-token = linecache.getline('Fonzie-Bot\Authentication Keys\Authentication.txt', 8)
+# Giphy Implementation ----------------------------------------------------------------------------------------------------------------------------
+giphyKey = linecache.getline('Fonzie-Bot\Authentication Keys\Authentication.txt', 17).rstrip()
 
-# Clean up token so it has no trailing newline
-token.rstrip()
+@bot.command(pass_context=True)
+async def giphy(ctx, *, search):
+    embed = discord.Embed(colour=discord.Colour.blue())
+    session = aiohttp.ClientSession()
+
+    # If the search is empty return a random gif
+    if search == '':
+        response = await session.get('https://api.giphy.com/v1/gifs/random?api_key=' + giphyKey)
+        data = json.loads(await response.text())
+        embed.set_image(url=data['data']['images']['original']['url'])
+    elif search != '': # If the search is not empty then implement it into the search url
+        search.replace(' ', '+')
+        response = await session.get('http://api.giphy.com/v1/gifs/search?q=' + search + '&api_key=' + giphyKey + '&limit=10')
+        data = json.loads(await response.text())
+        gif_choice = random.randint(0, 9)
+        embed.set_image(url=data['data'][gif_choice]['images']['original']['url'])
+
+    await session.close()
+    await ctx.send(embed=embed)
+
+
+# Detector ----------------------------------------------------------------------------------------------------------------------------------------
+detectorAnswers = ['ITS OFF THE SCALE!!!', 'Its getting a bit sus my guy.', 'None Detected.']
+
+@bot.command()
+async def detect(ctx, user: discord.Member, arg):
+    randNum = random.randint(3)
+    if user:
+        if randNum == 0:
+            # await ctx.send("hello, {}".format(user.mention))
+            await ctx.send('{}\'s {}: {}'.format(user.mention, arg, detectorAnswers[0]))
+        elif randNum == 1:
+            # await ctx.send("goodbye, {}".format(user.mention))
+            await ctx.send('{}\'s {}: {}'.format(user.mention, arg, detectorAnswers[1]))
+        elif randNum == 2:
+            # await ctx.send("goodbye, {}".format(user.mention))
+            await ctx.send('{}\'s {}: {}'.format(user.mention, arg, detectorAnswers[2]))
+    else:
+        await ctx.send('You have to mention someone numnutz')
+    
+
+# Code to get token for bot -----------------------------------------------------------------------------------------------------------------------
+token = linecache.getline('Fonzie-Bot\Authentication Keys\Authentication.txt', 8).rstrip()
 
 bot.run(token)
